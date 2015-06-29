5210fedfa734d230d07e526cdadab7824f6a0bff
Rion Dooley <dooley@tacc.utexas.edu>
2015-06-29 18:56:26 -0500

    * Fixed bug in config file where environment parsing was failing. * Updating tomcat_deploy script to explicitly refer to the script directory when creating deployment paths.

--- tomcat_deploy.sh
+++ tomcat_deploy.sh
@@ -9,25 +9,26 @@
 # mvn -Dskip.integration.tests=true -Dmaven.test.skip=true -Dforce.check.update=false -Dforce.check.version=false -s config/maven/settings.xml clean install
 # mvn -Dforce.check.version=false clean install
 
+DIR=$( cd "$( dirname "$0" )" && pwd )
 # deploy swagger docs to apach web root
 mkdir /var/www/html/v2
 chmod -R 755 /var/www/html/v2
 
 rm -rf /var/www/html/v2/docs
-ln -s /home/iplant/agave/agave-apidocs/apidocs-api/target/classes /var/www/html/v2/docs
+ln -s $DIR/agave-apidocs/apidocs-api/target/classes /var/www/html/v2/docs
 chmod -R 755 /var/www/html/v2/docs
 
 # deploy php apps to apache web root
 for i in auth postits logging; do
 	rm -rf /var/www/html/v2/$i
-	ln -s /home/iplant/agave/agave-$i/$i-api/target/classes /var/www/html/v2/$i
+	ln -s $DIR/agave-$i/$i-api/target/classes /var/www/html/v2/$i
 	chmod -R 755 /var/www/html/v2/$i
 done
 
 # deploy java webapps to tomcat
 for i in apps jobs files metadata monitors notifications profiles systems transforms; do
 	rm -rf $CATALINA_HOME/webapps/$i* $CATALINA_HOME/webapps/work/Catalina/localhost/$i
-	cp agave-$i/$i-api/target/*.war $CATALINA_HOME/webapps/;
+	cp $DIR/agave-$i/$i-api/target/*.war $CATALINA_HOME/webapps/;
 done
 
 # bounce tomcat
