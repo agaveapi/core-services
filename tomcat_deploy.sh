a7e268f9c37eef149fa2f83fbdc6fdb22d4da044
Rion Dooley <dooley@tacc.utexas.edu>
2015-06-29 18:23:48 -0500

    Updating tomcat deploy script.

--- tomcat_deploy.sh
+++ tomcat_deploy.sh
@@ -6,8 +6,8 @@
 #
 # git submodule update
 #
- mvn -Dskip.integration.tests=true -Dmaven.test.skip=true -Dforce.check.update=false -Dforce.check.version=false -s config/maven/settings.xml clean install
-#mvn -Dskip.integration.tests=true -Dmaven.test.skip=true -Dforce.check.update=false -Dforce.check.version=false clean install
+# mvn -Dskip.integration.tests=true -Dmaven.test.skip=true -Dforce.check.update=false -Dforce.check.version=false -s config/maven/settings.xml clean install
+# mvn -Dforce.check.version=false clean install
 
 # deploy swagger docs to apach web root
 mkdir /var/www/html/v2
